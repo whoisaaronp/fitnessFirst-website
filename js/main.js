f2d373c95c23b62be28ff9f87a89ed0dea52c07b
Aaron Paterson <juniha.aaron@gmail.com>
2019-08-16 10:03:19 -0400

    new boxes sketeton

--- js/main.js
+++ js/main.js
@@ -1,3 +1,33 @@
 // JavaScript Document
 
-console.log("Linked up");
+console.log("firing up");
+
+/* Using vanilla JavaScript */
+//  Hey scrollbar, animate the thing I’ll tell you when I’ll tell you.
+// Init ScrollMagic
+var ctrl = new ScrollMagic.Controller({
+    globalSceneOptions: {
+        triggerHook: 'onLeave'
+    }
+    
+});
+
+// Create scene
+$("section").each(function() {
+    // look for this id and set an indicator for each section
+    var name = $(this).attr('id');
+ 
+    new ScrollMagic.Scene({
+        triggerElement: this
+    })
+    .setPin(this)
+    .addIndicators({
+        colorStart: "rgba(255,255,255,0.5)",
+        colorEnd: "rgba(255,255,255,0.5)", 
+        colorTrigger : "rgba(255,255,255,1)",
+        name:name
+    })
+    .loglevel(3)
+    .addTo(ctrl);
+ 
+});
